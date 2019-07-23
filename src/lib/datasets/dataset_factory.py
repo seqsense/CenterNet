4706e3b1ccd1b19ee43d0103d32f9caac7dee53a
Hiroki Shibata <s17097@tokyo.kosen-ac.jp>
2019-07-23 17:15:04 +0900

    add files

--- src/lib/datasets/dataset_factory.py
+++ src/lib/datasets/dataset_factory.py
@@ -6,14 +6,17 @@ from .sample.ddd import DddDataset
 from .sample.exdet import EXDetDataset
 from .sample.ctdet import CTDetDataset
 from .sample.multi_pose import MultiPoseDataset
+from .sample.bbox_sample import BBoxTaskDataset
 
 from .dataset.coco import COCO
 from .dataset.pascal import PascalVOC
 from .dataset.kitti import KITTI
 from .dataset.coco_hp import COCOHP
+from .dataset.bbox_dataset import BBOX_DATA
 
 
 dataset_factory = {
+  'bbox_dataset': BBOX_DATA,
   'coco': COCO,
   'pascal': PascalVOC,
   'kitti': KITTI,
@@ -23,6 +26,7 @@ dataset_factory = {
 _sample_factory = {
   'exdet': EXDetDataset,
   'ctdet': CTDetDataset,
+  'bbox_sample': BBoxTaskDataset,
   'ddd': DddDataset,
   'multi_pose': MultiPoseDataset
 }
